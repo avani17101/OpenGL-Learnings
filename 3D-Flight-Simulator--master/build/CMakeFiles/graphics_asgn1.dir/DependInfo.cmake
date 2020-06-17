67b01ba4e01893df7697ffeab4a7ba7300601486
avani17101 <staravani1999@gmail.com>
2020-06-17 13:57:40 +0530

    added openGL tut + boilerplate code

---
+++ 3D-Flight-Simulator--master/build/CMakeFiles/graphics_asgn1.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/src/ball.cpp" "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/build/CMakeFiles/graphics_asgn1.dir/src/ball.cpp.o"
+  "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/src/bomb.cpp" "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/build/CMakeFiles/graphics_asgn1.dir/src/bomb.cpp.o"
+  "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/src/cannon_ball.cpp" "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/build/CMakeFiles/graphics_asgn1.dir/src/cannon_ball.cpp.o"
+  "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/src/checkpoint.cpp" "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/build/CMakeFiles/graphics_asgn1.dir/src/checkpoint.cpp.o"
+  "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/src/color.cpp" "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/build/CMakeFiles/graphics_asgn1.dir/src/color.cpp.o"
+  "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/src/compass.cpp" "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/build/CMakeFiles/graphics_asgn1.dir/src/compass.cpp.o"
+  "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/src/dashboard.cpp" "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/build/CMakeFiles/graphics_asgn1.dir/src/dashboard.cpp.o"
+  "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/src/fuel.cpp" "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/build/CMakeFiles/graphics_asgn1.dir/src/fuel.cpp.o"
+  "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/src/input.cpp" "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/build/CMakeFiles/graphics_asgn1.dir/src/input.cpp.o"
+  "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/src/main.cpp" "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/build/CMakeFiles/graphics_asgn1.dir/src/main.cpp.o"
+  "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/src/missile.cpp" "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/build/CMakeFiles/graphics_asgn1.dir/src/missile.cpp.o"
+  "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/src/nonedit.cpp" "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/build/CMakeFiles/graphics_asgn1.dir/src/nonedit.cpp.o"
+  "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/src/other_handlers.cpp" "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/build/CMakeFiles/graphics_asgn1.dir/src/other_handlers.cpp.o"
+  "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/src/parachute.cpp" "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/build/CMakeFiles/graphics_asgn1.dir/src/parachute.cpp.o"
+  "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/src/ring.cpp" "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/build/CMakeFiles/graphics_asgn1.dir/src/ring.cpp.o"
+  "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/src/sea.cpp" "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/build/CMakeFiles/graphics_asgn1.dir/src/sea.cpp.o"
+  "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/src/ship.cpp" "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/build/CMakeFiles/graphics_asgn1.dir/src/ship.cpp.o"
+  "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/src/specialenemy.cpp" "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/build/CMakeFiles/graphics_asgn1.dir/src/specialenemy.cpp.o"
+  "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/src/timer.cpp" "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/build/CMakeFiles/graphics_asgn1.dir/src/timer.cpp.o"
+  "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/src/volcano.cpp" "/home/lenovo/Desktop/OpenGL-Learnings/3D-Flight-Simulator--master/build/CMakeFiles/graphics_asgn1.dir/src/volcano.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/libdrm"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
